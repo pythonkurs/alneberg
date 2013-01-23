b633aba2d83a6068f1a8ede8ad8634dc481cda1b
Johannes Alneberg <alneberg@kth.se>
2013-01-23 11:40:45 +0100

    Files created when uploading to testpypi

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='alneberg',
+      version=version,
+      description="Pythonkurs code",
+      long_description="""\
+Just a silly one""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='Python Scilifelab',
+      author='Johannes Alneberg',
+      author_email='johannes.alneberg@scilifelab.se',
+      url='example.com',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
