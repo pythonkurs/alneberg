8760682ce1cd60a4475eeec910d3e2adbecddb2e
Johannes Alneberg <alneberg@kth.se>
2013-01-23 17:19:44 +0100

    Added the script getting_data.py to the setup

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ Just a silly one""",
       url='example.com',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      scripts=["scripts/getting_data.py"],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
