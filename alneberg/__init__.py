05fa0f3b8987970a01c89cf9b9153be4e427bf6e
Johannes Alneberg <alneberg@kth.se>
2013-01-23 17:11:52 +0100

    basic structure in the module for fetching NYC escalator data and counting repair status

--- alneberg/__init__.py
+++ alneberg/__init__.py
@@ -1,1 +1,22 @@
-#
+import untangle
+import requests
+
+def fetch_xml(url):
+    r = requests.get(url)
+    return r.text
+
+
+def fraction_escalator_with_repair_status(xml_raw):
+    doc = untangle.parse(xml_raw)
+    outages = doc.NYCOutages.outage
+    escalator_count = 0
+    repair_escalator_count = 0
+    
+    for outage in outages:
+        if outage.equipmenttype.cdata == 'ES':
+            escalator_count += 1
+            if outage.reason.cdata == 'REPAIR':
+                repair_escalator_count += 1
+    
+    fraction = repair_escalator_count/float(escalator_count)        
+    return fraction
