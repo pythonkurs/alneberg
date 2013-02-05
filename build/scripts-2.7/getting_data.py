4860c5a68dc9a1598fb4a55596ba63d964d0f32f
Johannes Alneberg <alneberg@kth.se>
2013-02-05 17:45:18 +0100

    Removed dist, build and .egg files

--- build/scripts-2.7/getting_data.py
+++
@@ -1,6 +0,0 @@
-#!/home/johannes/.virtualenvs/py2.7/bin/python
-from alneberg import fetch_NYC_escalator_info, fraction_escalator_with_repair_status
-
-xml = fetch_NYC_escalator_info()
-fraction_escalators = fraction_escalator_with_repair_status(xml)
-print fraction_escalators
