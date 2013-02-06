86bb47f2e563b421da9608776977e6d5665613fa
Johannes Alneberg <alneberg@kth.se>
2013-02-06 11:16:44 +0100

    Added check repo as a script in setyp.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ Just a silly one""",
       url='example.com',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      scripts=["scripts/getting_data.py"],
+      scripts=["scripts/getting_data.py", "scripts/check_repo.py"],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
