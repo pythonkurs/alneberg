9197370c8f84dfd9660c1c724952eb5221369093
Johannes Alneberg <alneberg@kth.se>
2013-02-06 18:01:02 +0100

    session4 can now print a dataframe with all commits and their time

--- alneberg/session4.py
+++ alneberg/session4.py
@@ -1,4 +1,5 @@
 import requests
+from pandas import DataFrame, Series
 
 def do_this():
     with open("/home/johannes/secret") as secret:
@@ -6,10 +7,18 @@ def do_this():
         
     repos = requests.get("https://api.github.com/orgs/pythonkurs/repos", auth=("alneberg", password))
     repos_data = repos.json()
+    data_dict = {}
     for repo in repos_data:
         git_commits_url = repo[u'commits_url'][0:-6]
         commits = requests.get(git_commits_url, auth=("alneberg", password))
         commits_data = commits.json()
+        commit_list = []
+        time_list = []
+        author_name = repo['html_url'].split('/')[-1]
         for commit in commits_data:
-            print commit['commit']['message'], commit['commit']['author']['date']
+            commit_list.append(commit['commit']['message'])
+            time_list.append(commit['commit']['author']['date'])
+        data_dict[author_name] = Series(commit_list, index=time_list)
+    df = DataFrame(data_dict)
+    print df
 
