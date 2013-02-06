410240c2cd66eb225ac5c1d7ac9156c80d3adce2
Johannes Alneberg <alneberg@kth.se>
2013-02-06 10:17:13 +0100

    Check function in CourseRepo now works

--- alneberg/session3.py
+++ alneberg/session3.py
@@ -1,10 +1,11 @@
+import os
 class CourseRepo(object):
     
     def __init__(self, surname):
         self.required = [".git", 
                          "setup.py", 
                          "README.md",
-                         "scripts/getting_data", 
+                         "scripts/getting_data.py", 
                          "scripts/check_repo.py",
                          "%s/__init__.py" % surname,
                          "%s/session3.py" % surname]
@@ -18,8 +19,14 @@ class CourseRepo(object):
         self.required = [".git",
                          "setup.py",
                          "README.md",
-                         "scripts/getting_data",
+                         "scripts/getting_data.py",
                          "scripts/check_repo.py",
                          "%s/__init__.py" % value,
                          "%s/session3.py" % value]
 
+
+    def check(self):
+        mi_b = True
+        for path in self.required:
+            mi_b = mi_b and os.path.exists(path)
+        return mi_b
