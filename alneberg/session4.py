edfb7746ba612c77cac55c52057d679096c91098
Johannes Alneberg <alneberg@kth.se>
2013-02-06 18:44:03 +0100

    Can calculate most common day and hour of commiting

--- alneberg/session4.py
+++ alneberg/session4.py
@@ -1,7 +1,10 @@
 import requests
 from pandas import DataFrame, Series
+from dateutil import parser
+import datetime
+from collections import Counter
 
-def do_this():
+def fetch_commits():
     with open("/home/johannes/secret") as secret:
         password = secret.read().strip()
         
@@ -20,5 +23,27 @@ def do_this():
             time_list.append(commit['commit']['author']['date'])
         data_dict[author_name] = Series(commit_list, index=time_list)
     df = DataFrame(data_dict)
-    print df
+    return df
+#    df.to_csv('mydf.tsv', sep='\t')
 
+def reload_df(file_name):
+    df = read_table(file_name)
+    return df
+
+def common_commit_times(df):
+    weekday_hash = {0: "Monday", 1: "Tuesday",
+                    2: "Wednesday", 3: "Thursday",
+                    4: "Friday", 5: "Saturday",
+                    6: "Sunday"}
+    time_list = list(df.index)
+    day_list = []
+    hour_list = []
+    for time_str in time_list:
+        time = parser.parse(time_str)
+        day_list.append(time.weekday())
+        hour_list.append(time.hour)
+    c_day = Counter(day_list)
+    c_hour = Counter(hour_list)
+    day_name = weekday_hash[c_day.most_common(1)[0][0]]
+    mc_hour = c_hour.most_common(1)[0][0]
+    return day_name,mc_hour
